947a3e7d8531eca5180a41882114a9be3efd8052
awesome.however <awesome.however@yandex.ru>
2023-04-17 15:31:25 +0300

    Apply custom template

--- src/App.js
+++ src/App.js
@@ -1,60 +1,36 @@
 import React from 'react';
-import ToDoTask from './ToDoTask';
 import ToDoTaskAdd from './ToDoTaskAdd';
 import ToDoList from './ToDoList';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
-
+import { todoAddAll } from './actions'
+import { Provider, connect } from 'react-redux';
 
 class App extends React.Component {
-	constructor(props) {
-    super(props);
-    
-    this.state = {
-      tasks: []
-    }
-    this.onTaskDelete = this.onTaskDelete.bind(this); 
-	this.onTaskAdd = this.onTaskAdd.bind(this); 
-  }
   
   componentDidMount() {
-    console.log('componentDidMount');
-
     fetch('tasks').then(function(res) {
       return res.json();
     }).then((data) => { 
-         this.setState({
-           tasks: data
-         });
-    });
-  }
-
-  onTaskAdd(task) {
-    this.setState({
-      tasks: [...this.state.tasks, task]
-    });
-  }
-  
-  onTaskDelete(_id) {
-    this.setState({
-      tasks: this.state.tasks.filter(function(task) {
-        return task._id !== _id;
-      })
+		 this.props.dispatch(todoAddAll(data));
     });
   }
 
   render(){
     return (
-      <div className="App">
-        <Router>
-		  <Routes>
-		    <Route path="/" element={<ToDoList tasks={this.state.tasks} onTaskDelete={this.onTaskDelete} />} />
-			<Route path="/add" element={<ToDoTaskAdd onTaskAdd={this.onTaskAdd} />} />
-		  </Routes>
-		</Router>
-      </div>
+      <div className="row d-flex justify-content-center container">
+    <div className="col-md-8">
+			  <Provider store={this.props.store}>
+				<Router>
+				  <Routes>
+					<Route path="/" element={<ToDoList />} />
+					<Route path="/add" element={<ToDoTaskAdd />} />
+				  </Routes>
+				</Router>
+			  </Provider>
+		</div>
+	  </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
